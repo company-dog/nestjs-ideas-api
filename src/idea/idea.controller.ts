e80432d547ab7d4fac5bf162ab748b9fd738e113
Yuichi Nagakura <yuichi.nagakura@asia-quest.jp>
2020-02-14 18:27:07 +0900

    add idea crud api

---
+++ src/idea/idea.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { IdeaService } from './idea.service';
+import { IdeaDto } from './idea.dto';
+
+@Controller('idea')
+export class IdeaController {
+  constructor(private readonly ideaService: IdeaService) {}
+  @Get()
+  showAllIdeas() {
+    return this.ideaService.showAll();
+  }
+
+  @Post()
+  createIdea(@Body() data: IdeaDto) {
+    return this.ideaService.create(data);
+  }
+
+  @Get(':id')
+  readIdea(@Param('id') id: string) {
+    return this.ideaService.read(id);
+  }
+
+  @Put(':id')
+  updateIdea(@Param('id') id: string, @Body() data: Partial<IdeaDto>) {
+    return this.ideaService.update(id, data);
+  }
+
+  @Delete(':id')
+  destroyIdea(@Param('id') id: string) {
+    return this.ideaService.destroy(id);
+  }
+}
